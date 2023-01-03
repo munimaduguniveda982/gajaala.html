cb6885de53d1f2c39dcf490f1bdaba5497f5fa60
munimaduguniveda01 <121536079+munimaduguniveda01@users.noreply.github.com>
2023-01-03 06:12:49 -0800

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def gajaala(request):
+    return render(request,'gajaala.html')
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def venkey(request):
+    return render(request,'venkey.html')
